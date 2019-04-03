5b87c47f5e4ce35332de69aa951fcba6a4a410d1
vladimirpezzole <vladimir.hipermidia@gmail.com>
2019-04-03 15:21:01 -0300

    + Cap 03

--- DatascienceAcademy/PythonADS/Cap03/Lab02/calculadora_v1.py
+++ DatascienceAcademy/PythonADS/Cap03/Lab02/calculadora_v1.py
@@ -6,3 +6,62 @@
 
 print("\n******************* Python Calculator *******************")
 
+# Funções matemáticas
+
+def add(x, y): #Soma
+	return x + y
+
+def subtract(x, y): #Subtração
+	return x - y
+
+def multiply(x, y): #Multiplicação
+	return x * y
+
+def divide(x, y): #Divisão
+	return x / y
+
+def pot(x, y): #Potência
+	return x ** y
+
+print("\n***** Selecione o número da operação desejada: ****\n")
+print("1 - Soma")
+print("2 - Subtração")
+print("3 - Multiplicação")
+print("4 - Divisão")
+print("5 - Potência")
+
+#variável
+escolha = input("\nDigite sua opção (1/2/3/4/5): ")
+
+#entrada de valores
+num1 = int(input("\nDigite o primeiro número: "))
+num2 = int(input("\nDigite o segundo número: "))
+
+#Seleção de operação
+if escolha == '1':
+	print("\n")
+	print(num1, "+", num2, "=", add(num1, num2))
+	print("\n")
+
+elif escolha == '2':
+	print("\n")
+	print(num1, "-", num2, "=", subtract(num1, num2))
+	print("\n")
+
+elif escolha == '3':
+	print("\n")
+	print(num1, "*", num2, "=", multiply(num1, num2))
+	print("\n")
+
+elif escolha == '4':
+	print("\n")
+	print(num1, "/", num2, "=", divide(num1, num2))
+	print("\n")
+    
+elif escolha == '5':
+	print("\n")
+	print(num1, "^", num2, "=", pot(num1, num2))
+	print("\n")
+
+else:
+	print("\nOpção Inválida!")
